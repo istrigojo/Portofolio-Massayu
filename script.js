ec4382bf0ed21b4d15acf292c5093cef487bbc4f
Jacob <jacobjokey@gmail.com>
2024-04-25 19:46:05 +0700

    minor change

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(".navbar-nav").on("click", "a", function () {
         $(".navbar-nav a.active").removeClass("active");
-          $(this).addClass("active");
+            $(this).addClass("active");
     });
 });
 
